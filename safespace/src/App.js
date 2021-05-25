0fdc4fc2b4537bf2cf38fa7ae9571d7a9a3bdcfe
AmritanshPandey <amritansh.pandey6@gmail.com>
2021-05-25 21:55:43 +0530

    initial setup

---
+++ safespace/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+          <Header />
+    </div>
+  );
+}
+
+export default App;
