a9b1239e6f840c1728c4ed44b7821561d9c7b414
AmritanshPandey <amritansh.pandey6@gmail.com>
2021-06-23 22:25:12 +0530

    added pages and re-structured

--- safespace/src/pages/home.js
+++ safespace/src/pages/home.js
@@ -1,7 +1,65 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { gsap } from "gsap";
+import Banner from "../components/banner";
+import Blogs from "../components/blogs";
+import IntroOverlay from "../components/introOverlay";
+
+const tl = gsap.timeline();
+
+const homeAnimation = (completeAnimation) => {
+  tl.from(".line span", 1.8, {
+    y: 100,
+    ease: "power4.out",
+    delay: 1,
+    skewY: 7,
+    stagger: {
+      amount: 0.3,
+    },
+  })
+    .to(".overlay-top", 1.6, {
+      height: 0,
+      ease: "expo.inOut",
+      stagger: 0.4,
+    })
+    .to(".overlay-bottom", 1.6, {
+      width: 0,
+      ease: "expo.inOut",
+      delay: -0.8,
+      stagger: {
+        amount: 0.4,
+      },
+    })
+    .to(".intro-overlay", 0, { css: { display: "none" } })
+    .from(".blog-image img", 1.6, {
+      scale: 1.4,
+      ease: "expo.inOut",
+      delay: -2,
+      stagger: {
+        amount: 0.4,
+      },
+      onComplete: completeAnimation,
+    });
+};
 
 const Home = () => {
-  return <div></div>;
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const completeAnimation = () => {
+    setAnimationComplete(true);
+  };
+
+  useEffect(() => {
+    // on load time
+    homeAnimation(completeAnimation);
+  }, []);
+  return (
+    <>
+      {animationComplete === false ? <IntroOverlay /> : ""}
+
+      <Banner />
+      <Blogs />
+    </>
+  );
 };
 
 export default Home;
