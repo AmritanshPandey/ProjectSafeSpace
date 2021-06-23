1f93f194039b90fdccd093cbbd4cc35dbdd6487f
AmritanshPandey <amritansh.pandey6@gmail.com>
2021-06-23 23:47:57 +0530

    navigation funtional

--- safespace/src/App.js
+++ safespace/src/App.js
@@ -1,8 +1,28 @@
 import { useEffect } from "react";
 import { gsap } from "gsap";
-import Header from "./components/header";
-import Home from "./pages/home";
+import { Route } from "react-router-dom";
+// CSS
 import "./styles/App.scss";
+
+//components
+
+import Header from "./components/header";
+import Navigation from "./components/navigation";
+
+//pages
+import BlogList from "./pages/blogsList";
+import About from "./pages/about";
+import GetInTouch from "./pages/getIntouch";
+import Home from "./pages/home";
+
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/blogs", name: "Home", Component: BlogList },
+  { path: "/about", name: "Home", Component: About },
+  { path: "/getintouch", name: "Home", Component: GetInTouch },
+];
+
+//routes
 
 function App() {
   useEffect(() => {
@@ -15,7 +35,14 @@ function App() {
   return (
     <>
       <Header />
-      <Home />
+      <div className="App">
+        {routes.map(({path, Component}) => (
+          <Route key={path} exact path={path}>
+            <Component />
+          </Route>
+        ))}
+      </div>
+      <Navigation />
     </>
   );
 }
